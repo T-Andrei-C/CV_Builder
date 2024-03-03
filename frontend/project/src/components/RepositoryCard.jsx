a5316b24c78e05ec798b1eeb6bd36a49482d0011
Andrew <toroaba_andrei@yahoo.com>
2024-03-03 11:01:15 +0200

    design on repository card almost finished

--- frontend/project/src/components/RepositoryCard.jsx
+++ frontend/project/src/components/RepositoryCard.jsx
@@ -1,9 +1,20 @@
 import {useEffect, useState} from "react";
-import {getAllRepositoryLanguages, getReadMeForRepository} from "../service/CRUDGitRepository";
+import {
+    getAllContributorsFromRepository,
+    getAllRepositoryLanguages, getNumberOfCommitsFromRepository,
+    getReadMeForRepository
+} from "../service/CRUDGitRepository";
+
+import {IoIosArrowUp} from "react-icons/io";
+import {LuArrowUpRightFromCircle} from "react-icons/lu";
+import {IoTriangleSharp} from "react-icons/io5";
 
 const RepositoryCard = ({gitRepository, languagesColors}) => {
     const [repositoryLanguages, setRepositoryLanguages] = useState(null);
     const [repositoryReadMeImage, setRepositoryReadMeImage] = useState(null);
+    const [repositoryContributors, setRepositoryContributors] = useState(null);
+    const [commitCount, setCommitCount] = useState(null);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     useEffect(() => {
         getAllRepositoryLanguages(gitRepository.name).then(languages => {
@@ -15,42 +26,76 @@ const RepositoryCard = ({gitRepository, languagesColors}) => {
         }).catch(error => {
             setRepositoryReadMeImage("Not found");
         })
-    }, []);
 
-    // function capture(){
-    //     var canvas = document.getElementById('canvas');
-    //     var video = document.getElementById('video');
-    //     canvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-    // }
+        getAllContributorsFromRepository(gitRepository.name).then(contributors => {
+            setRepositoryContributors(contributors);
+        })
 
-    // console.log(createThumb("https://github.com/CodecoolGlobal/epg-3-java-T-Andrei-C/assets/115529065/cdc2dd99-4d6a-4d72-85da-e981f892818e", 100, 100));
+        getNumberOfCommitsFromRepository(gitRepository.name).then(commitCount => {
+            setCommitCount(commitCount);
+        });
+    }, [commitCount]);
 
     return (
+
         <div className="m-2 h-min w-min">
-            <div className="relative">
-                <p className="mt-3 ms-5 px-2 pb-1 rounded bg-black absolute text-white font-medium text-xl">{gitRepository.name}</p>
+            <div className="relative border-2 border-gray-100 rounded-t-lg">
+                <a href={gitRepository.html_url} target="_blank" className="absolute flex justify-end bg-black opacity-0 w-full h-full transition duration-300 ease-in rounded-t-lg hover:opacity-60 cursor-pointer">
+                    <h1 className="text-white m-2 me-3 text-xl"><LuArrowUpRightFromCircle /></h1>
+                </a>
+                <div className="absolute inline-flex">
+                    <p className="mt-2 ps-1 block text-black pb-0.5 relative bg-gray-100 font-medium w-full h-min">{gitRepository.name}</p>
+                    <p className="text-gray-100 mt-3 relative rotate-90 text-[1.8em] transform -translate-x-[0.1em] -translate-y-[0.18em]"><IoTriangleSharp /></p>
+                </div>
                 <img
                     src={repositoryReadMeImage}
-                    onError={(e) => e.target.src = "https://media.discordapp.net/attachments/920704942765903922/1212742763112570900/OIP.png?ex=65f2f1b6&is=65e07cb6&hm=c96b57caf88f8cffb417417e0c29710025fc263cc37f82a732a0ff524c510044&=&format=webp&quality=lossless&width=592&height=592"}
+                    onError={(e) => e.target.src = "https://media.discordapp.net/attachments/920704942765903922/1213503472385654814/Untitled430_20240302170922.png?ex=65f5b62d&is=65e3412d&hm=b5eb939c0fd603d68eebb41733aa5502ae785a2816302abba512b369efb2246e&=&format=webp&quality=lossless&width=610&height=610"}
                     alt={gitRepository.name}
                     className="object-cover rounded-t-lg h-60 w-60 sm:h-72 sm:w-72"/>
             </div>
 
             <div
                 className="mt-1 rounded-b-lg bg-white border-black h-40 w-60 sm:w-72 shadow-lg inline-grid grid-cols-2">
-                <div className="">
-
-                </div>
-                <div className="flex flex-wrap justify-end h-min">
+                <div className="relative w-28 sm:w-[8.5rem] h-[8.5rem]">
+                    <button
+                        className="ms-1 mt-1 flex text-gray-400 justify-between bg-white text-xs w-full font-bold rounded-sm border-gray-100 border hover:bg-gray-100 transition duration-300 ease-in"
+                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                        <p className="ms-0.5">Contributors</p>
+                        <p className={isDropdownOpen ? "me-0.5 mt-0.5 transition rotate-180 duration-500" : "me-0.5 mt-0.5 transition rotate-0 duration-500"}><IoIosArrowUp/></p>
+                    </button>
                     {
-                        repositoryLanguages && repositoryLanguages.map(language => (
-                            <p className="border px-1 pb-0.5 m-1 w-min h-min rounded font-bold text-xs"
-                               style={{
-                                   borderColor: languagesColors[language]?.color,
-                                   color: languagesColors[language]?.color
-                               }}>{language}</p>
-                        ))
+                        isDropdownOpen &&
+                        <div
+                            className="ms-1 mt-1 bg-white absolute text-xs rounded-sm shadow-sm w-28 sm:w-[8.5rem] divide-y divide-gray-50">
+                            {
+                                repositoryContributors.map(contributor => (
+                                    <a href={contributor.html_url} target="_blank"
+                                       className="flex justify-between cursor-pointer hover:bg-gray-50 transition delay-75 ease-in">
+                                        <img alt={contributor.name} src={contributor.avatar_url}
+                                             className="my-0.5 ms-0.5 w-4 rounded"/>
+                                        <p className="my-0.5 me-0.5">{contributor.login}</p>
+                                    </a>
+                                ))
+                            }
+                        </div>
                     }
+                    <p className="m-1 px-1 border border-black w-max font-bold text-xs rounded-sm">{commitCount} Commits</p>
+                </div>
+                <div className="h-[8.5rem]">
+                    <div className="flex flex-wrap justify-end h-min">
+                        {
+                            repositoryLanguages && repositoryLanguages.map(language => (
+                                <p className="border px-1 pb-0.5 m-1 w-min h-min rounded font-bold text-xs"
+                                   style={{
+                                       borderColor: languagesColors[language]?.color,
+                                       color: languagesColors[language]?.color
+                                   }}>{language}</p>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="text-center font-thin text-xs w-60 sm:w-72">
+                    <h1>Last updated on {gitRepository.pushed_at.substring(0, 10)}</h1>
                 </div>
             </div>
         </div>
--- frontend/project/src/service/CRUDGitRepository.jsx
+++ frontend/project/src/service/CRUDGitRepository.jsx
@@ -1,10 +1,10 @@
 
-const token = "Bearer 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";
+const auth = `Bearer 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`;
 
 export const getAllGitRepositories = async () => {
     const request = await fetch("https://api.github.com/users/T-Andrei-C/repos", {
         headers: {
-            "Authorization" : token
+            "Authorization" : auth
         }
     });
     return await request.json();
@@ -13,7 +13,7 @@ export const getAllGitRepositories = async () => {
 export const getAllRepositoryLanguages = async (repositoryName) => {
     const request = await fetch(`https://api.github.com/repos/T-Andrei-C/${repositoryName}/languages`, {
         headers: {
-            "Authorization" : token
+            "Authorization" : auth
         }
     });
     return await request.json();
@@ -22,10 +22,31 @@ export const getAllRepositoryLanguages = async (repositoryName) => {
 export const getReadMeForRepository = async (repositoryName) => {
     const request = await fetch(`https://api.github.com/repos/T-Andrei-C/${repositoryName}/readme`, {
         headers: {
-            "Authorization" : token
+            "Authorization" : auth
         }
     })
     return await request.json();
+}
+
+export const getAllContributorsFromRepository = async (repositoryName) => {
+    const request = await fetch(`https://api.github.com/repos/T-Andrei-C/${repositoryName}/contributors`, {
+        headers: {
+            "Authorization" : auth
+        }
+    })
+    return await request.json();
+}
+
+export const getNumberOfCommitsFromRepository = async (repositoryName) => {
+    const request = await fetch(`https://api.github.com/repos/T-Andrei-C/${repositoryName}/commits?per_page=1&page=1`, {
+        headers: {
+            "Authorization" : auth
+        }
+    })
+
+    const pageLink = request.headers.get("Link").split("page");
+    const commitNumber = pageLink[pageLink.length - 1].substring(1).replace(">; rel=\"last\"", "");
+    return commitNumber;
 }
 
 export const getGitColorsForLanguages = async () => {
